e40e35871d2dce95bedeca4b8e9dbe4d3fc8230b
kimown <kimown@163.com>
2022-11-25 15:52:26 +0800

    Update V8Inc.h

--- Dependencies/v8inspector/Include/V8Inc.h
+++ Dependencies/v8inspector/Include/V8Inc.h
@@ -4,9 +4,9 @@
 #pragma warning(push)
 #pragma warning(disable: 4100 4267 4127)
 #endif
-
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <v8.h>
-#include <v8-inspector.h>
+#include </usr/include/nodejs/deps/v8/include/v8-inspector.h>
 #include <libplatform/libplatform.h>
 
 #ifndef __clang__
